5ef9a6154e9dbb8d786f6dd80f3bedc4bb3b519f
Joshua Rabinek <43835737+jrabi002@users.noreply.github.com>
2020-12-07 14:40:40 -0800

    Code completed

--- main.cpp
+++ main.cpp
@@ -19,11 +19,24 @@ BusOut sseg_bus(segA, segB, segC, segD, segE, segF, segG);
 //Array for the display of numbers 0-3
 uint8_t sseg[4] = {0x3F, 0x06, 0x5B, 0x4F};
 
+//Button Input
+DigitalIn button(PTA2);
+volatile uint8_t button_pressed = 0;
+
 //number of sensors used
 #define TOTAL_PARKING_SPACES 3
 
 //Task ticker for task functions
 Ticker tasktimer;
+
+//Counters to blink sseg when no spot is available (25ms increments)
+#define BLINK_COUNT 5
+#define BLINK_TIMER 20
+uint8_t blink_sseg_timer = BLINK_TIMER;
+uint8_t blink_sseg_count = BLINK_COUNT;
+uint8_t blink_sseg_on = 0;
+uint8_t sseg_on = 1;
+
 
 //Sensor slave addresses
 #define SLAVE_ADDR_1A 0x1A << 1
@@ -54,13 +67,17 @@ volatile uint8_t sensor_update = 0;
 //external interrupt inputs (from sensors)
 InterruptIn sensor_1A(PTD0);
 InterruptIn sensor_1B(PTD1);
-InterruptIn sensor_1C(PTD1);
+InterruptIn sensor_1C(PTD2);
+
+//interrupt for button input
+InterruptIn button_press(PTA2);
 
 //sensor function declarations
 void sensors_init(const uint8_t*);
 void task_sensor_update(volatile uint8_t*, const uint8_t*);
-
-void display_sseg(uint8_t);
+void sseg_update(void);
+void find_closest_parking_space(uint8_t*, const uint8_t*);
+void blink_sseg_led(void);
 
 //Sensor external interupt ISRs
 void ISR1A()
@@ -84,19 +101,40 @@ void timer_tick()
     timerflag = 1;
 }
 
+void ISR_button()
+{
+    if (!button_pressed)
+        button_pressed = 1;
+}
+
 int main()
 {
+    //Set I2C frequency
     s.frequency(400000);
     
     //Interupt Tasks (get update from sensor)
     sensor_1A.rise(&ISR1A);
-    //sensor_1B.rise(&ISR1B);
-    //sensor_1C.rise(&ISR1C);
+    sensor_1B.rise(&ISR1B);
+    sensor_1C.rise(&ISR1C);
+
+    //Button configuration use internal pullup resistors
+    //Inverse Logic
+    button.mode(PullUp);
+    
+    //wait 1ms for pullup
+    wait_us(1000);
+    
+    //Interupt Task for button
+    button_press.fall(&ISR_button);
     
     //Ticker for 50ms task executions
-    tasktimer.attach(&timer_tick, 5ms);
+    tasktimer.attach(&timer_tick, 25ms);
 
     const uint8_t sensors[3] = {SLAVE_ADDR_1A, SLAVE_ADDR_1B, SLAVE_ADDR_1C};
+    
+    //delay to wait for sensors to startup (1s)
+    wait_us(1000000);
+
     sensors_init(sensors);
 
     while (true) {
@@ -105,24 +143,32 @@ int main()
         if(sensor_update)
         {
             task_sensor_update(sensor_to_check, sensors);
-            display_sseg(TOTAL_PARKING_SPACES - sensor_status[0] - sensor_status[1] - sensor_status[2]);
+            sseg_update();
             sensor_update = 0;
         }
+        if (button_pressed == 1) 
+        {
+            find_closest_parking_space(sensor_status, sensors);
+            button_pressed = 0;
+        }
+        if (blink_sseg_on == 1)
+            blink_sseg_led();
+            
         timerflag = 0;
     }
 }
 
-void display_sseg(uint8_t digit)
+void sseg_update(void)
 {
-    sseg_bus = sseg[digit];
+    sseg_bus = sseg[TOTAL_PARKING_SPACES - sensor_status[0] - sensor_status[1] - sensor_status[2]];
 }
 
 void sensors_init(const uint8_t* sensor_addr)
 {
     sensor_status[0] = s.get_status(sensor_addr[0]) ? 1 : 0;
-    //sensor_status[1] = sensor.get_status(sensor_addr[1]) ? 1 : 0;
-    //sensor_status[2] = sensor.get_status(sensor_addr[2]) ? 1 : 0;
-    display_sseg(TOTAL_PARKING_SPACES - sensor_status[0] - sensor_status[1] - sensor_status[2]);
+    sensor_status[1] = s.get_status(sensor_addr[1]) ? 1 : 0;
+    sensor_status[2] = s.get_status(sensor_addr[2]) ? 1 : 0;
+    sseg_update();
 }
 
 void task_sensor_update(volatile uint8_t* sensor_to_check, const uint8_t* sensor_addr)
@@ -144,4 +190,46 @@ void task_sensor_update(volatile uint8_t* sensor_to_check, const uint8_t* sensor
         sensor_status[2] = s.get_status(sensor_addr[2]) ? 1 : 0;
         sensor_to_check[2] = 0;
     }    
+}
+
+void find_closest_parking_space(uint8_t* sensor_status, const uint8_t* sensor_addr)
+{
+    if (sensor_status[0] == 0)
+        s.start_blink_led(sensor_addr[0]);
+    else if (sensor_status[1] == 0)
+        s.start_blink_led(sensor_addr[1]);
+    else if (sensor_status[2] == 0)
+        s.start_blink_led(sensor_addr[2]);
+    else 
+        blink_sseg_on = 1;
+}
+
+void blink_sseg_led(void)
+{
+    if (blink_sseg_timer > 0)
+        blink_sseg_timer--;
+    if (blink_sseg_timer == 0)
+    {
+        if (blink_sseg_count > 0)
+            {
+                sseg_on ^= 1;
+                if (sseg_on)
+                    sseg_update();
+                else 
+                    sseg_bus = 0;
+            blink_sseg_count--;
+            }
+        blink_sseg_timer = BLINK_TIMER;
+    }
+    if (blink_sseg_count == 0)
+    {
+        blink_sseg_on = 0;
+        blink_sseg_count = BLINK_COUNT;
+        blink_sseg_timer = BLINK_TIMER;
+        if (sseg_on == 0)
+        {
+            sseg_on = 1;
+            sseg_update();
+        }
+    }
 }
